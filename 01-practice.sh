d10466f075ffade1ab9209c6fa1abdb83c951c70
Sai0205 <deepak.yeddalareddy@gmail.com>
2024-11-06 20:38:32 +0530

    first commit

--- 01-practice.sh
+++ 01-practice.sh
@@ -3,4 +3,8 @@
 p1=deepak
 p2=suneel
 
-echo "$p1 and $p2"
+echo "$p1 and $p2" #refering the variable using $
+
+DATE=$(date) #asssigning the command output to value
+DATES=$(ls -la)
+echo "$DATE $DATES"
