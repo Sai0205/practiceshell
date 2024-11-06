f6f5b24153d4c1d09a0abb9579e43ad1f573318f
Sai0205 <deepak.yeddalareddy@gmail.com>
2024-11-06 20:51:48 +0530

    first commit

---
+++ condition.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $(USERID) -ne 0 ]
+then  
+    echo "please run with root user"
+    exit 1
+fi    
+
+yum install mysql -y
+
+if [ $? -ne 0 ]
+then
+    echo "installation of mysql is error"
+    exit 1
+else 
+    echo "sucess"
+fi   
+
+yum install postfix -y
+
+if [ $? -ne 0 ]
+then 
+    echo "error postfix"
+    exit 1
+else  
+    echo "sucess"
+fi  
+
